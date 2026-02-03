8fb41bd6371e12b2f894c9399e5819add387778b
ramhindustanispro-crypto <ramhindustanispro@gmail.com>
2026-02-03 20:04:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://intelligent-angela-motionprime-b9551ad0.koyeb.app/")
+URL = environ.get("URL", "")
 
 
 # Don't Remove Credit Tg - @VJ_Bots
